12d8db2aab3a6f164557f9b9def098e6e3454fcf
kashif-flask <70099921+kashif-flask@users.noreply.github.com>
2020-09-11 20:37:35 +0530

    Create glo.py

---
+++ glo.py
@@ -0,0 +1,5 @@
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.models import KeyedVectors
+glove2word2vec('glove.6B.100d.txt','glove.6B.100d.txt.word2vec')
+model=KeyedVectors.load_word2vec_format('glove.6B.100d.txt.word2vec',binary=False)
+model.save("model.bin")
