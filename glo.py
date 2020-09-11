4e3fa75655eb8baa11aa3cfed7cf367a475d2283
kashif-flask <70099921+kashif-flask@users.noreply.github.com>
2020-09-11 20:41:30 +0530

    Update glo.py

--- glo.py
+++ glo.py
@@ -1,5 +1,5 @@
 from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.models import KeyedVectors
-glove2word2vec('glove.6B.100d.txt','glove.6B.100d.txt.word2vec')
+glove2word2vec('glove.6B.100d.txt','glove.6B.100d.txt.word2vec') //'glove.6B.100d.txt this file need to be downloaded from glove official site' 
 model=KeyedVectors.load_word2vec_format('glove.6B.100d.txt.word2vec',binary=False)
 model.save("model.bin")
